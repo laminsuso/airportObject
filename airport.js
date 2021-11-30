bfc193878d642bf433397d6817a6b5fcbaf342ae
Lamin Suso <susolamin@hotmail.com>
2021-11-30 10:55:05 -0600

    added a JSON file

--- airport.js
+++ airport.js
@@ -1,16 +1,18 @@
-class airport{
-    constructor(terminal){
-        this.terminal = terminal
-    }
-}
-
-class airplane extends airport{
-    constructor(Terminal, seat, wings){
-        this.wings = wings
+const fs = require('fs')
+class Airport{
+    constructor(name){
+        this.name =name
     }
 
-    Fly(){
-        console.log(" I can fly with the help of my {this.wings}")
+       getInfo(callback) {
+        //read the json file
+        fs.readFile('./airportsData.json', (err, data) =>{
+            //return all airports as json object
+            const airports = JSON.parse(data)
+            //find airport whose iata code matches this airport name
+            const airport = airports.find(airport => airport.iata === this.name)
+            callback(err,airport)
+        })
     }
 }
-module.exports = airport
+module.exports = Airport
--- airport.test.js
+++ airport.test.js
@@ -1,8 +1,24 @@
-const airport = require('./airport')
+const Airport = require('./airport')
+
 
 describe('airport class', () =>{
     test('has a terminal', () =>{
-        const testTerminal = new airport("terminal")
-        expect (testTerminal.terminal).toBe("terminal")
+        const testTerminal = new Airport("terminal")
+        expect (testTerminal.name).toBe("terminal")
+    })
+    
+    test('airports have a country', (done) => {
+        const testAirport2 = new Airport("CDG")
+        //run getInfo method
+        testAirport2.getInfo((err, info) => {
+        //log this airport info to console
+        console.log(info)
+        //expect error to be null
+        expect(err).toBeNull()
+        //expect the airport country to be FR
+        expect(info.country).toEqual('FR')
+        //end asynchrounous test
+        done()
+        })
     })
 })
